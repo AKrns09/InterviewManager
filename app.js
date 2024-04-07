c6ef46ac25f2dd0ea96fc675af040bcbf5405135
Aaron Kearns <x20513033@student.ncirl.ie>
2024-04-07 23:45:15 +0100

    secure update

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use('/', indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+  res.setHeader('X-XSS-Protection','1;mode=block');
+  res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self'");
 });
 
 // error handler
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var MySql = require('sync-mysql');
 var connection_details = require("../modules/connection_details")
+var { validator, body } = require('express-validator');
+var crypto = require('crypto');
+var secretKey = 'my-secret-key';
 
 router.get('/', function(req, res, next) {
   var connection = new MySql({
@@ -23,7 +26,7 @@ router.get('/search', function(req, res, next) {
     })
     var query = req.query.query
     query = "%" + query  + "%"
-    var candidates = connection.query('select * from candidates where candidate_id = ?', [query]);
+    var candidates = connection.query('select * from candidates where candidate_id like (?);', [query]);
     res.render('index', {title: 'Search Results', candidates: candidates, page_header: 'Search Results', link: '/', link_name: "Searched candidate"})
 });
 
@@ -38,17 +41,21 @@ router.get('/add', function(req, res, next){
   res.render('add_candidate', { candidates: candidates} )
 });
 
-router.post("/add", function(req, res, next) {
+router.post("/add", body('candidate_name').escape(), body('cv_score').escape(), body('interviewer').escape(), function(req, res, next) {
   var candidate_name = req.body.candidate_name
   var cv_score = req.body.cv_score
   var interviewer = req.body.interviewer
+  //Reference: https://bootcamp.uxdesign.cc/understanding-hashing-encoding-and-encryption-in-express-js-2fec834a7d9e
+  var cipher = crypto.createCipher('aes-256-cbc', secretKey);
+  let encrypted_cv_score = cipher.update(cv_score, 'utf-8', 'hex');
+  encrypted_cv_score += cipher.final('hex');
   var connection = new MySql({
     user: 'root',
     password: 'ryte11/seRt62',
     host: connection_details.host,
     database: connection_details.database
   });
-  connection.query("INSERT INTO candidates(candidate_name, cv_score, interviewer) VALUES ((?), (?), (?));", [candidate_name, cv_score, interviewer]);
+  connection.query("INSERT INTO candidates(candidate_name, cv_score, interviewer) VALUES ((?), (?), (?));", [candidate_name, encrypted_cv_score, interviewer]);
   console.log(req.body)
   res.redirect("/")
 })
